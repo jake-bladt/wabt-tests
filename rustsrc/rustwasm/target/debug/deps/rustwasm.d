945569f8f9d9debac379cf0159897cd0d246704c
Jake Bladt <jake@jakebladt.com>
2019-11-30 00:51:02 -0500

    Save artifacts

---
+++ rustsrc/rustwasm/target/debug/deps/rustwasm.d
@@ -0,0 +1,5 @@
+D:\code\wabt-tests\rustsrc\rustwasm\target\debug\deps\rustwasm.dll: src\lib.rs
+
+D:\code\wabt-tests\rustsrc\rustwasm\target\debug\deps\rustwasm.d: src\lib.rs
+
+src\lib.rs:
---
+++ rustsrc/rustwasm/target/debug/rustwasm.d
@@ -0,0 +1,1 @@
+D:\code\wabt-tests\rustsrc\rustwasm\target\debug\rustwasm.dll: D:\code\wabt-tests\rustsrc\rustwasm\src\lib.rs
